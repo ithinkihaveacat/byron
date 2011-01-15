1f37c5a5802e9a1a1f8669d7d757f033020a3938
Michael Stillwell <mjs@beebo.org>
2011-01-15 21:52:14 +0000

    Added docs, removed requirement that "Service" be appended.

--- lib/Broker.php
+++ lib/Broker.php
@@ -2,6 +2,31 @@
 
 namespace Byron;
 
+/**
+ * The Broker system is merely a more structured way of creating a no-argument function
+ * for each service that returns an object appropriately configured (with correct hostnames,
+ * usernames, passwords, etc.) for interacting with that service.
+ *
+ * That is, the constructor for a low-level library function will probably have arguments
+ * like hostname, username, password, etc.  It's incovenient and annoying to have to
+ * figure out how and where these details should come from every time you want to use
+ * the service.  You could create a big PHP file with a function for each service:
+ *
+ *     function getMySQLService() {
+ *         // extract connection details from a (global?) variable
+ *         $service = ...
+ *         return $service;
+ *     }
+ *
+ * But this is a bit messy an unclean.  The alternative is to create a "Broker"
+ * object, giving it a mechanism to create an appropriate class names for a service,
+ * when that service is requested.  Then the broker can instantiate that class, passing
+ * it any information it might need, like an object storing configuration details.
+ * Then, the broker could call a "getService()" method on the newly-instantiated object,
+ * returning the return value of "getService()" to the caller.  This is the mechanism
+ * implemented by this class.
+ */
+
 class Broker {
 
     protected static $instance = null;
@@ -48,8 +73,8 @@ class Broker {
 
     public function __call($name, $args) {
 
-        if (!preg_match("/^get(\w*)Service$/", $name, $matches)) {
-            throw new \Exception("only calls of the form [getXxxService()] are supported (not [$name()])");
+        if (!preg_match("/^get(\w*)(Service?)$/", $name, $matches)) {
+            throw new \Exception("only calls of the form [getXxx()] are supported (not [$name()])");
         }
 
         // TODO Implement some caching?
