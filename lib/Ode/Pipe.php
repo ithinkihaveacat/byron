7d808741e7ad69c2316d1c4ab5171abf83add378
Michael Stillwell <mjs@beebo.org>
2011-02-06 16:19:40 +0000

    Improve docs

--- lib/Ode/Pipe.php
+++ lib/Ode/Pipe.php
@@ -6,7 +6,7 @@ interface Pipe
 {
     
     // Has to be pass by reference because some systems (XSL) don't modify the document
-    // in place, so we have to do it for them.
+    // in place, so we have to do it for them by assigning to $doc.
     public function __invoke(&$doc, $args = array()); 
     
 };
