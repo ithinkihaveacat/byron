457a311524a8a48735c406eea7c1e23a714436f1
Michael Stillwell <mjs@beebo.org>
2011-03-03 00:36:04 +0000

    Enhance bootstrap so that it can find Zend_*.

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -6,3 +6,31 @@
 
 require_once('PHPUnit/Framework.php');
 require_once(__DIR__ . '/../byron.php');
+
+// Arrange for Zend_* to be found.  Assumed to be installed at the
+// same level as "byron" itself.
+
+spl_autoload_register(function ($klass) {
+
+    // Map $klass to the $filename it's supposed to be defined in
+    
+    if (strpos($klass, "Zend_") === 0) {
+        
+        $filename = sprintf("%s/../../%s.php", __DIR__, str_replace("_", "/", $klass));
+
+        if (is_readable($filename)) { // can use stream_resolve_include_path() to search include path
+            require_once($filename);
+        } 
+        
+    }
+    
+});
+
+set_include_path(join(PATH_SEPARATOR, array(
+    __DIR__ . "/../../",
+    get_include_path()
+)));
+
+// Helper functions
+
+blib('raw');
