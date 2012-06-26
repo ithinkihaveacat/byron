b0e2e95e3e8d5c24312f4582c5e3fcf4e71254ee
Michael Stillwell <mjs@beebo.org>
2012-06-26 23:09:32 +0100

    Switch to composer for ZF dependency
    Let's see how much of a pain this is...

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -6,6 +6,7 @@
 
 require_once('PHPUnit/Autoload.php');
 require_once(__DIR__ . '/../byron.php');
+require_once(__DIR__ . '/../vendor/autoload.php');
 
 // Arrange for Zend_* to be found.  Assumed to be installed at the
 // same level as "byron" itself, in a directory called "zend-framework".
@@ -22,12 +23,6 @@ spl_autoload_register(function ($klass) {
     }
     
 });
-
-set_include_path(join(PATH_SEPARATOR, array(
-    __DIR__ . "/../../zend-framework/library",
-    __DIR__ . "/../zend-framework/library", # For http://travis-ci.org
-    get_include_path()
-)));
 
 // Helper functions
 
