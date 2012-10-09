a13c79c7e43cec28e2dd6efa79b8e8a5477f73b4
Michael Stillwell <mjs@beebo.org>
2012-10-09 11:35:42 +0100

    Rename toNamedEntities() to toHtmlEntities()
    Add some tests.

--- tests/small/StringTest.php
+++ tests/small/StringTest.php
@@ -7,6 +7,7 @@ class StringTest extends PHPUnit_Framework_Testcase
     {
         $s = new \Byron\String("Hello, World!");
         
+        $this->assertEquals("Hello, World!", $s->toHtmlEntities());
         $this->assertEquals("Hello, World!", $s->toNamedEntities());
         $this->assertEquals("Hello, World!", $s->toNumericEntities());
     }
@@ -16,9 +17,21 @@ class StringTest extends PHPUnit_Framework_Testcase
         $s = new \Byron\String("Hello, <World>!");
         
         $this->assertEquals("Hello, &#60;World&#62;!", $s->toNumericEntities());
+        $this->assertEquals("Hello, &lt;World&gt;!", $s->toHtmlEntities());
         $this->assertEquals("Hello, &lt;World&gt;!", $s->toNamedEntities());
+        $this->assertEquals("Hello, &lt;World&gt;!", $s->toXmlEntities());
     }
-    
+
+    public function testSpecialCharacters()
+    {
+        $s = new \Byron\String("Some of my favourite characters as &, <, >, £, ¶, # and '");
+
+        $this->assertEquals("Some of my favourite characters as &#38;, &#60;, &#62;, &#163;, &#182;, # and '", $s->toNumericEntities());
+        $this->assertEquals("Some of my favourite characters as &amp;, &lt;, &gt;, &pound;, &para;, # and '", $s->toHtmlEntities());
+        $this->assertEquals("Some of my favourite characters as &amp;, &lt;, &gt;, &pound;, &para;, # and '", $s->toNamedEntities());
+        $this->assertEquals("Some of my favourite characters as &amp;, &lt;, &gt;, £, ¶, # and '", $s->toXmlEntities());
+    }
+
     public function testToUri()
     {
         $s = new \Byron\String("Hello, World!");
